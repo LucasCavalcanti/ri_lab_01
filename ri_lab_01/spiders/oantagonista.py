6b03aceb344335d0180622f4cfcaf1db7dd47ee4
LucasCavalcanti <lucascavalcanti223@gmail.com>
2019-04-05 17:28:11 -0300

    add scrapy request

--- ri_lab_01/spiders/oantagonista.py
+++ ri_lab_01/spiders/oantagonista.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
+import datetime
 
 from ri_lab_01.items import RiLab01Item
 from ri_lab_01.items import RiLab01CommentItem
@@ -10,6 +11,8 @@ class OantagonistaSpider(scrapy.Spider):
     name = 'oantagonista'
     allowed_domains = ['oantagonista.com']
     start_urls = []
+    start_page = 1
+    limit_date = datetime.datetime(2019,1,1)
 
     def __init__(self, *a, **kw):
         super(OantagonistaSpider, self).__init__(*a, **kw)
@@ -18,21 +21,34 @@ class OantagonistaSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
-        for quote in response.css('div.container-post-home'):
-            yield {
-                'title': quote.css('a.article_link h2::text').get(),
-                'date': quote.css('a.article_link span time::text').get(),
-                'section': quote.css('a.article_link span span::text').get(),
-                'text': quote.css('a.article_link p::text').get()
-            }
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        def checkDate(response, lowerLimit):
+            currentDate = response.css('time.entry-date ::attr(datetime)').get()
+            currentDate = currentDate.split(' ')[0].split('-')
+
+            date =  datetime.datetime(int(currentDate[-3]), int(currentDate[-2]), int(currentDate[-1]))
+
+            return date > lowerLimit
+
+        if (checkDate(response, self.limit_date) and self.start_page <= 2):
+            for quote in response.css('div.container-post-home'):
+                yield {
+                    'title': quote.css('a.article_link h2::text').get(),
+                    'date': quote.css('a.article_link span time::text').get(),
+                    'section': quote.css('a.article_link span span::text').get(),
+                    'author': quote.css('a.article_link span div::text').get(),
+                    'text': quote.css('a.article_link p::text').get(),
+                    'url': quote.css('a.article_link::attr("href")').get()
+                }
+            nextPage = self.start_urls[0] + 'pagina/' + str(self.start_page) +'/?ajax'
+            self.start_page = self.start_page + 1
+
+            page = response.url.split("/")[-2]
+            filename = 'quotes-%s.html' % page
+            with open(filename, 'wb') as f:
+                f.write(response.body)
+            self.log('Saved file %s' % filename)
+
+            yield scrapy.Request(nextPage, callback=self.parse)
         #
         #
         #
